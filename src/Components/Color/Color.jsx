a808804a09feb899dbac9e0cf92f1d335e5b7ea6
Tia Nguyen <trang@live.de>
2024-05-03 09:17:11 +0200

    delete-color

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color({ color, onDeleteColor }) {
+  const [approveDelete, setApproveDelete] = useState(false);
+
+  function handleDelete() {
+    if (approveDelete === false) {
+      setApproveDelete(true);
+    } else {
+      onDeleteColor(color.id);
+      setApproveDelete(false);
+    }
+  }
+
+  function handleCancel() {
+    setApproveDelete(false);
+  }
+
   return (
     <div
       className="color-card"
@@ -9,9 +25,28 @@ export default function Color({ color }) {
         color: color.contrastText,
       }}
     >
-      <h3 className="color-card-headline">{color.hex}</h3>
+      <h3 className="color-card-highlight">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {approveDelete ? (
+        <>
+          <p>
+            Really delete?
+            <button type="button" onClick={handleCancel}>
+              CANCEL
+            </button>
+            <button type="button" onClick={handleDelete}>
+              DELETE
+            </button>
+          </p>
+        </>
+      ) : (
+        <>
+          <button type="button" onClick={handleDelete}>
+            DELETE
+          </button>
+        </>
+      )}
     </div>
   );
 }
