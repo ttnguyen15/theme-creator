cf2ab538ca5ecd5f96066189aaff6fdba3e94509
Tia Nguyen <trang@live.de>
2024-05-06 11:42:03 +0200

    copy to clipboard

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Color.css";
 import ColorForm from "../ColorForm/ColorForm";
+import { writeClipboardText } from "./CopyToClipboard.jsx";
 
 export default function Color({ color, onDeleteColor, onEditColor }) {
   const [deleteMode, setDeleteMode] = useState(false);
   const [editMode, setEditMode] = useState(false);
+  const [copyMode, setCopyMode] = useState(false);
 
   function handleDelete() {
     if (deleteMode) {
@@ -22,6 +24,23 @@ export default function Color({ color, onDeleteColor, onEditColor }) {
     setEditMode(false);
   }
 
+  function handleCopy() {
+    setCopyMode(true);
+    writeClipboardText(color.hex);
+  }
+
+  useEffect(() => {
+    let timeoutId;
+    if (copyMode) {
+      timeoutId = setTimeout(() => {
+        setCopyMode(false);
+      }, 3000);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [copyMode]);
+
   return (
     <div
       className="color-card"
@@ -31,6 +50,9 @@ export default function Color({ color, onDeleteColor, onEditColor }) {
       }}
     >
       <h3 className="color-card-headline">{color.hex}</h3>
+      <button onClick={handleCopy}>
+        {copyMode ? "SUCCESSFULLY COPIED!" : "COPY"}
+      </button>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
       {editMode ? (
